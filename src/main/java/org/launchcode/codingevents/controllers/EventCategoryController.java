f305e82b63f7e2baf9599f39423f993c99343be6
Masterlynx2 <Tonywatkins325@gmail.com>
2022-03-07 20:15:58 -0500

    studio march 7

--- src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -17,34 +17,31 @@ import javax.validation.Valid;
 public class EventCategoryController {
 
     @Autowired
-   private EventCategoryRepository eventCategoryRepository;
+    private EventCategoryRepository eventCategoryRepository;
 
-
-    @GetMapping("index")
-    public String displayAllCategories(Model model ){
-        model.addAttribute("title","All Categories");
-        model.addAttribute("categories",eventCategoryRepository.findAll());
+    @GetMapping
+    public String displayAllCategories(Model model) {
+        model.addAttribute("title", "All Categories");
+        model.addAttribute("categories", eventCategoryRepository.findAll());
         return "eventCategories/index";
     }
 
     @GetMapping("create")
-    public String renderCreateEventCategoryForm(Model model){
+    public String renderCreateEventCategoryForm(Model model) {
         model.addAttribute("title", "Create Category");
         model.addAttribute(new EventCategory());
-        return"eventCategories/create";
+        return "eventCategories/create";
     }
 
     @PostMapping("create")
-    public String processCreateEventCategoryForm( @Valid @ModelAttribute EventCategory  eventCategory,
-                                                              Errors errors ,Model model ){
+    public String processCreateEventCategoryForm(@Valid @ModelAttribute EventCategory eventCategory,
+                                                 Errors errors, Model model) {
 
-       if(errors.hasErrors()) {
-
-
-           model.addAttribute("title", "Create Category");
-           model.addAttribute(new EventCategory());
-           return"eventCategories/create";
-       }
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Create Category");
+            model.addAttribute(new EventCategory());
+            return "eventCategories/create";
+        }
 
         eventCategoryRepository.save(eventCategory);
         return "redirect:";
--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -8,30 +8,18 @@ import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity
-public class EventCategory {
+public class EventCategory extends AbstractEntity{
 
-    @Id
-    @GeneratedValue
-    private int id;
 
-    @Size(min = 3, message = "must be at least 3 characters long")
+
+    @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
 
-    public EventCategory(@Size(min = 3, message = "name must be at least 3 characters long") String name) {
-
+    public EventCategory(@Size(min = 3, message = "Name must be at least 3 characters long") String name) {
         this.name = name;
     }
 
-    public EventCategory() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
+    public EventCategory() {}
 
     public String getName() {
         return name;
@@ -41,21 +29,12 @@ public class EventCategory {
         this.name = name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof EventCategory)) return false;
-        EventCategory that = (EventCategory) o;
-        return id == that.id;
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 
     @Override
     public String toString() {
         return name;
     }
+
+
 }
